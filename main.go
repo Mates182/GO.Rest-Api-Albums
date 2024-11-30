eebbc7fdd70ed0635ab69b0d7f386dd4fabf0dd0
MateoPillajo <98783413+Mates182@users.noreply.github.com>
2024-11-30 11:46:58 -0500

    feat: :hammer: Create a GET method
    Create a GET method for /albums

--- main.go
+++ main.go
@@ -15,7 +15,10 @@ func handler(c *gin.Context) {
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
+
 	router.GET("/", handler)
+	router.GET("/albums", getAlbums)
+
 	router.Run("localhost:8080")
 }
 
@@ -30,6 +33,14 @@ type album struct {
 // Album data
 var albums = []album{
 	{ID: "1", Title: "Born to Die", Artist: "Lana del Rey", Year: 2012},
-	{ID: "2", Title: "Born to Die", Artist: "Lana del Rey", Year: 2012},
-	{ID: "3", Title: "Born to Die", Artist: "Lana del Rey", Year: 2012},
+	{ID: "2", Title: "Froot", Artist: "Marina and the Diamonds", Year: 2015},
+	{ID: "3", Title: "I've IVE", Artist: "IVE", Year: 2023},
+	{ID: "4", Title: "Silence Between Songs", Artist: "Madison Beer", Year: 2023},
+	{ID: "5", Title: "2", Artist: "(G)I-DLE", Year: 2024},
+}
+
+// API REST
+// GET Albums
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
 }
