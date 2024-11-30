991be633b3d6f4ad20433e0260e815e923822111
MateoPillajo <98783413+Mates182@users.noreply.github.com>
2024-11-30 11:28:30 -0500

    feat: :hammer: Update main.go with Album data
    Add an album structure and album data on memory

--- main.go
+++ main.go
@@ -11,9 +11,25 @@ func handler(c *gin.Context) {
 		"title": "GO REST API",
 	})
 }
+
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
 	router.GET("/", handler)
 	router.Run("localhost:8080")
+}
+
+// Album structure
+type album struct {
+	ID     string `json:"id"`
+	Title  string `json:"title"`
+	Artist string `json:"artist"`
+	Year   int    `json:"year"`
+}
+
+// Album data
+var albums = []album{
+	{ID: "1", Title: "Born to Die", Artist: "Lana del Rey", Year: 2012},
+	{ID: "2", Title: "Born to Die", Artist: "Lana del Rey", Year: 2012},
+	{ID: "3", Title: "Born to Die", Artist: "Lana del Rey", Year: 2012},
 }
